75141821a14a824b7271407e64128e222e00ffb5
Toni <tbahcic@tvz.hr>
2020-11-15 23:42:36 +0100

    Add responsive footer

--- src/main/webapp/app/shared/layout/footer/footer.tsx
+++ src/main/webapp/app/shared/layout/footer/footer.tsx
@@ -7,8 +7,8 @@ import { Col, Row } from 'reactstrap';
 const Footer = props => (
   <div className="footer page-content">
     <Row>
-      <Col md="12">
-        <p>Your footer</p>
+      <Col lg={{size: 8, offset: 2}} md={{size: 10, offset: 1}} sm={{size: 12, offset: 0}}>
+        <p>Contact: <b>tbahcic@tvz.hr</b></p>
       </Col>
     </Row>
   </div>
