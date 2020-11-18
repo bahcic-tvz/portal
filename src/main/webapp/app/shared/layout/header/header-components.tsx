c5966b2f9b876f643d79f384d0bc2f77d9c55552
Toni <tbahcic@tvz.hr>
2020-11-18 22:55:33 +0100

    add logo

--- src/main/webapp/app/shared/layout/header/header-components.tsx
+++ src/main/webapp/app/shared/layout/header/header-components.tsx
@@ -8,7 +8,7 @@ import appConfig from 'app/config/constants';
 
 export const BrandIcon = props => (
   <div {...props} className="brand-icon">
-    <img src="content/images/portal-logo.svg" alt="Portal logo" />
+    <img src="content/images/logo.png" alt="Portal logo"/>
   </div>
 );
 
